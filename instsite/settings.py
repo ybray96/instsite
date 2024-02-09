6ac9d1dfbe88a280f33358235ef69d60925aefc0
ybray96 <51830133+ybray96@users.noreply.github.com>
2024-02-09 14:26:47 +0600

    add global port

--- instsite/settings.py
+++ instsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-a1y0-^dp1=d!uni#i++!g^=w^zzdgx5jz0$6degnt6-60ri7nt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.18.0.1','91.147.92.207','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['172.18.0.1','91.147.92.207','localhost','127.0.0.1','91.147.92.207:7781']
 
 
 # Application definition
